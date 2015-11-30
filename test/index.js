11097503ad21efc37a8b811ab9175e5fb88b1b45
Samy Pessé <samypesse@gmail.com>
2015-11-30 17:01:09 +0100

    Add test for href option

--- test/index.js
+++ test/index.js
@@ -15,4 +15,17 @@ describe('superscript', function() {
                 assert.equal(result[0].content, '<h1 id="test-me">test me</h1>\n<p>Hello world. <sup>superscript text!</sup></p>')
             });
     });
+
+    it('should correctly include links when "href" property', function() {
+        return tester.builder()
+            .withContent('#test me \n\nHello world. {% sup href="https://google.com" %}superscript text!{% endsup %}')
+            .withBookJson({
+                plugins: ['superscript']
+            })
+            .withLocalPlugin(path.join(__dirname, '..'))
+            .create()
+            .then(function(result) {
+                assert.equal(result[0].content, '<h1 id="test-me">test me</h1>\n<p>Hello world. <sup><a href="https://google.com" target="_blank">superscript text!</a></sup></p>')
+            });
+    });
 });
