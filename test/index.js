e7db3266091d2d2715ab1146daea8cdfc55a9164
Tomas Dvorak <todvora@gmail.com>
2015-11-30 17:28:09 +0100

    no need of explicitly mentioned locally installed plugins in book.json

--- test/index.js
+++ test/index.js
@@ -6,9 +6,6 @@ describe('superscript', function() {
     it('should correctly replace by sup html tag', function() {
         return tester.builder()
             .withContent('#test me \n\nHello world. {% sup %}superscript text!{% endsup %}')
-            .withBookJson({
-                plugins: ['superscript']
-            })
             .withLocalPlugin(path.join(__dirname, '..'))
             .create()
             .then(function(result) {
